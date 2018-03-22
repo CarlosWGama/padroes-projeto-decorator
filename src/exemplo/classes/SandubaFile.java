7e5d7bc6cfca9ae8adc52a235c50bb1d3887b9f6
Carlos W. Gama <carloswgama@gmail.com>
2018-03-22 17:58:14 -0300

    Ajuste

--- src/exemplo/classes/SandubaFile.java
+++ src/exemplo/classes/SandubaFile.java
@@ -2,6 +2,8 @@ package exemplo.classes;
 
 public class SandubaFile extends Sanduba {
 
-	protected String ingredientes = "Pão Bola, File de Alcatra, Queijo Prata, Presunto, Ovo, Bancon, Alface, Tomate ";
-	protected double preco = 20.00;
+	public SandubaFile() {
+		ingredientes = "Pão Bola, File de Alcatra, Queijo Prata, Presunto, Ovo, Bancon, Alface, Tomate ";
+		preco = 20.00;
+	}
 }
--- src/exemplo/classes/SandubaMistao.java
+++ src/exemplo/classes/SandubaMistao.java
@@ -2,6 +2,8 @@ package exemplo.classes;
 
 public class SandubaMistao extends Sanduba {
 
-	protected String ingredientes = "Pão Bola, File de Alcatra, File de Frango, Queijo Prata, Presunto, Ovo, Bancon, Alface, Tomate ";
-	protected double preco = 22.00;
+	public SandubaMistao() {
+		ingredientes = "Pão Bola, File de Alcatra, File de Frango, Queijo Prata, Presunto, Ovo, Bancon, Alface, Tomate ";
+		preco = 22.00;
+	}
 }
